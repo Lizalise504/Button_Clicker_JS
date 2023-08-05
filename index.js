8b31547f8e1b1e6bd666727c59fb28e4e7bfce29
Lizalise <lizalisenompu@gmail.com>
2023-08-05 23:31:41 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// function to change login to logout when clicked
+function login(signin){
+    signin.innerText = "Logout";
+}
+
+// function to remove Add Definition
+function move(hide){
+    hide.remove();
+}
+
